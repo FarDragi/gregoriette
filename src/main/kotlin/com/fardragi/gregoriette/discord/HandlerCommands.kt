627a79af2c7470a3024b9611fb1c325fd841b0b6
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2024-06-14 20:03:13 -0300

    Update bot command handler

--- src/main/kotlin/com/fardragi/gregoriette/discord/HandlerCommands.kt
+++ src/main/kotlin/com/fardragi/gregoriette/discord/HandlerCommands.kt
@@ -4,7 +4,8 @@ import com.fardragi.gregoriette.discord.commands.IBaseCommand
 import com.fardragi.gregoriette.discord.commands.TpsCommand
 import dev.minn.jda.ktx.events.onCommand
 import dev.minn.jda.ktx.interactions.commands.restrict
-import dev.minn.jda.ktx.interactions.commands.upsertCommand
+import dev.minn.jda.ktx.interactions.commands.slash
+import dev.minn.jda.ktx.interactions.commands.updateCommands
 import net.dv8tion.jda.api.JDA
 
 class HandlerCommands(client: JDA) {
@@ -13,7 +14,9 @@ class HandlerCommands(client: JDA) {
       val commands = listOf<IBaseCommand>(TpsCommand())
 
       commands.forEach { command ->
-        client.upsertCommand(command.name(), command.description()) { restrict(true) }.queue()
+        client
+            .updateCommands { slash(command.name(), command.description()) { restrict(true) } }
+            .queue()
         client.onCommand(command.name()) { event -> command.exec(event) }
       }
     }
